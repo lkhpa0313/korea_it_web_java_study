e1533ded41aedabed5188bfb44479f1799620ec4
lhpa0313 <lkhpa@naver.com>
2025-05-26 14:41:09 +0900

    Feat: Operator

---
+++ src/_05_Operator/Operator02.java
@@ -0,0 +1,32 @@
+package _05_Operator;
+
+public class Operator02 {
+    public static void main(String[] args) {
+        // 증감 연산자
+        int j = 10;
+        System.out.println(j);
+
+        // 실행이 된 후에 증가
+        System.out.println(j++); //10 => 10 + 1
+        System.out.println(j);
+        //실행이 된 후에 1감소
+        System.out.println(j--);
+        System.out.println(j);
+        //실행이 되기 전에 1증가
+        System.out.println(++j); //11
+        //실행이 되기 전에 1감소
+        System.out.println(--j); //10
+
+        // 비교 연산자 => 결과가 항상 boolean 참/거짓 둘 중 하나
+        num1 = 5;
+        num2 = 3;
+        System.out.println(num1 > num2);
+        System.out.println(num1 >= num2);
+        System.out.println((num1 < num2));
+        System.out.println(num1 <= num2);
+        System.out.println(num1 == num2); // 같은 것이냐?
+        System.out.println(num1 != num2); // 같은 것이 아니냐?
+
+
+    }
+}
---
+++ src/_05_Operator/Operator03.java
@@ -0,0 +1,45 @@
+package _05_Operator;
+
+public class Operator03 {
+    public static void main(String[] args) {
+        //논리 연산자 (&, |, !)
+        boolean flag1 = true;
+        boolean flag2 = false;
+        System.out.println(flag1 & flag2); // & and 논리곱 => 하나라도 false가 있으면 false
+        System.out.println(flag1 | flag2); // | or 논리합 => 하나라도 true가 있으면 true
+        System.out.println(flag2 | flag2);
+        System.out.println(!flag1);
+
+        //논리 연산자 조합 (&&, ||)
+        //&& => 선 조건이 true일 때만 후 조건을 실행한다, 선 조건이 false이면 후 조건을 실행하지 않는다
+        //|| => 선 조건이 true이면 후조건을 실행하지 않는다. 선 조건이 false일 때만 후 조건을 실행한다.
+        System.out.println(flag1 && flag2);
+        System.out.println(flag1 || flag2);
+
+        // 삼항 연산자
+        int x = 5;
+        int y = 3;
+        int max = (x > y) ? x : y;
+        System.out.println(max);
+
+        int min = (x < y) ? x : y;
+        System.out.println(min);
+
+        x = 3;
+        boolean isSame = (x == y) ? true : false;
+        System.out.println(isSame);
+
+        String sameStr = (x != y) ? "다름" : "같음";
+        System.out.println(sameStr);
+
+        /*
+         * int height = 숫자;
+         * 키가 120이상 인 경우 탑승 가능하도록 하는 삼항연산자
+         * 가능하면 "탑승 가능" 붕가능하면 "탑승 불가능" 출력
+         * */
+
+        int height = 180;
+        String result = (height >= 120) ? "탑승 가능" : "탑승 불가능";
+        System.out.println(result);
+    }
+}
